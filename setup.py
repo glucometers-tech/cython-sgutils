c43d6b5c727d2b2b52d322710f4ae818b92ae765
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-03-25 17:33:51 +0000

    Move static descriptions to `setup.cfg` simplifying `setup.py`.
    This removes a number of fixed strings from the Python side of the setup,
    and prefers referencing the `README.md` file by name.

--- setup.py
+++ setup.py
@@ -7,24 +7,7 @@ from setuptools import Extension, find_packages, setup
 import setuptools_scm  # Ensure it's present.
 from Cython.Build import cythonize
 
-with open("README.md", "rt") as fh:
-    long_description = fh.read()
-
 setup(
-    name="cython-sgutils",
-    description="Cython-based bindings for libsgutils",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="Diego Elio Pettenò",
-    author_email="flameeyes@flameeyes.com",
-    url="https://github.com/glucometers-tech/cython-sgutils",
-    keywords=["scsi"],
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: MIT License",
-    ],
     packages=find_packages(),
     package_data={"": ["*.pyx", "*.pxd"]},
     ext_modules=cythonize(
